7247ef9d048e420b79b563170aa8dfe0dd5ee2bb
esther_chen <esther_chen@trendmicro.com>
2024-06-07 15:52:41 +0800

    fix test

--- app.py
+++ app.py
@@ -52,6 +52,6 @@ if __name__ == '__main__':
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
 
-    server = pywsgi.WSGIServer(('', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('https://websockets-echo-dh6t.onrender.com', HTTP_SERVER_PORT), app, handler_class=WebSocketHandler)
     print("Server listening on: http://localhost:" + str(HTTP_SERVER_PORT))
     server.serve_forever()
